b3bc31e5d89ee14828e6640a62e2eef5b0874c70
Timothy Tsung <98036889+tesung@users.noreply.github.com>
2022-06-14 22:28:09 -0400

    Update NFT_recommender.py

--- NFT_recommender.py
+++ NFT_recommender.py
@@ -6,7 +6,14 @@ st.set_page_config(page_title="Reddit Thread Searcher", layout="wide")
 title = "Reddit NFT Thread Recommender"
 st.title(title)
 
-df = pd.read_csv("recommender_df.csv")
+df_1 = pd.read_csv("recommender_df_1.csv")
+df_2 = pd.read_csv("recommender_df_2.csv")
+df_3 = pd.read_csv("recommender_df_3.csv")
+df_4 = pd.read_csv("recommender_df_4.csv")
+df_5 = pd.read_csv("recommender_df_5.csv")
+df_6 = pd.read_csv("recommender_df_6.csv")
+
+df = pd.concat([df_1,df_2,df_3,df_4,df_5,df_6])
     
 st.write('Welcome to my recommender system. Please choose a topic from the drop-down menu for several reddit link suggestions.')
 
