22223352afc0e7fe42f16b4b5278f4a98f3973cb
Timothy Tsung <98036889+tesung@users.noreply.github.com>
2022-06-14 22:34:52 -0400

    Update NFT_recommender.py

--- NFT_recommender.py
+++ NFT_recommender.py
@@ -15,7 +15,7 @@ df_6 = pd.read_csv("recommender_df_6.csv")
 
 df = pd.concat([df_1,df_2,df_3,df_4,df_5,df_6])
     
-st.write('Welcome to my recommender system. Please choose a topic from the drop-down menu for several reddit link suggestions.')
+st.write('Welcome to my recommender system. Please choose a topic from the drop-down menu for several reddit link suggestions. Once the link populates, copy & paste it into a search bar to visit the Reddit thread.')
 
 topics = ['Crypto Discussion','Metamask','Moderator Channel','NFT Art','NFT Buy/Sell General Discussion','NFT Theft','NFT Giveaway','Onlyfans','Opensea','Random']
 
